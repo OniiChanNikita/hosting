52b7f6544d8fba111ee6284726c87fd32dde9e8b
OniiChanNikita <rccomfy1461@gmail.com>
2023-02-21 02:10:54 +0100

    host

--- hosting/settings.py
+++ hosting/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
